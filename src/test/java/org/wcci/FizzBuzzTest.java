9802a5225bf6e8c8e702564158b767c1b9f71435
Benjamin Williams <ben@wecancodeit.org>
2020-02-10 12:16:12 -0500

    Created FizzBuzz and FizzBuzzTest

---
+++ src/test/java/org/wcci/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+package org.wcci;
+
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void oneShouldSayOne() {
+        FizzBuzz underTest = new FizzBuzz();
+    }
+}
