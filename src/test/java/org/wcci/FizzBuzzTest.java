6eb01077e034b579c9ad81e3381ce10db00aa780
Benjamin Williams <ben@wecancodeit.org>
2020-02-10 13:13:04 -0500

    Adding unit tests

--- src/test/java/org/wcci/FizzBuzzTest.java
+++ src/test/java/org/wcci/FizzBuzzTest.java
@@ -2,10 +2,89 @@ package org.wcci;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
 
     @Test
-    public void oneShouldSayOne() {
+    public void shouldInstantiate() {
         FizzBuzz underTest = new FizzBuzz();
     }
+
+//    @Test
+//    public void oneShouldSayOne() {
+//        FizzBuzz underTest = new FizzBuzz();
+//        String result = underTest.say(1);
+//        assertEquals("1", result);
+//    }
+//
+//    @Test
+//    public void twoShouldSayTwo() {
+//        FizzBuzz underTest = new FizzBuzz();
+//        String result = underTest.say(2);
+//        assertEquals("2", result);
+//    }
+//
+//    @Test
+//    public void threeShouldSayFizz() {
+//        FizzBuzz underTest = new FizzBuzz();
+//        String result = underTest.say(3);
+//        assertEquals("Fizz", result);
+//    }
+//
+//    @Test
+//    public void fourShouldSayFour() {
+//        FizzBuzz underTest = new FizzBuzz();
+//        String result = underTest.say(4);
+//        assertEquals("4", result);
+//    }
+//
+//    @Test
+//    public void fiveShouldSayBuzz() {
+//        FizzBuzz underTest = new FizzBuzz();
+//        String result = underTest.say(5);
+//        assertEquals("Buzz", result);
+//    }
+//    @Test
+//    public void sixShouldSayFizz() {
+//        FizzBuzz underTest = new FizzBuzz();
+//        String result = underTest.say(6);
+//        assertEquals("Fizz", result);
+//    }
+//    @Test
+//    public void sevenShouldSaySeven() {
+//        FizzBuzz underTest = new FizzBuzz();
+//        String result = underTest.say(7);
+//        assertEquals("7", result);
+//    }
+//    @Test
+//    public void eightShouldSayEight() {
+//        FizzBuzz underTest = new FizzBuzz();
+//        String result = underTest.say(8);
+//        assertEquals("8", result);
+//    }
+//    @Test
+//    public void nineShouldSayFizz() {
+//        FizzBuzz underTest = new FizzBuzz();
+//        String result = underTest.say(9);
+//        assertEquals("Fizz", result);
+//    }
+//    @Test
+//    public void tenShouldSayBuzz() {
+//        FizzBuzz underTest = new FizzBuzz();
+//        String result = underTest.say(10);
+//        assertEquals("Buzz", result);
+//    }
+//    @Test
+//    public void fifteenShouldSayFizzBuzz() {
+//        FizzBuzz underTest = new FizzBuzz();
+//        String result = underTest.say(15);
+//        assertEquals("FizzBuzz", result);
+//    }
+//    @Test
+//    public void ThirtyShouldSayFizzBuzz() {
+//        FizzBuzz underTest = new FizzBuzz();
+//        String result = underTest.say(30);
+//        assertEquals("FizzBuzz", result);
+//    }
 }
